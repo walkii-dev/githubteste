4aa3be1519f3431930ce8c1d02af8452cfc3ef8c
Lucas Oliveira <lucasp.oliveira822@gmail.com>
2025-02-13 09:36:23 -0300

    projeto inicial

---
+++ app.js
@@ -0,0 +1,83 @@
+//let cidade = 'São Paulo';
+
+//let altura = 1.75;
+
+//let numbOfProducts = 30;
+
+//let saldoTotal = 5000;
+
+//alert('Atenção! preencha todos os campos corretamente!');
+
+//let mensagemDeAviso = 'Atenção! Preencha todos os campos direitinho!';
+//alert(mensagemDeAviso);
+
+//let corFavorita = prompt('Qual sua cor favorita?');
+//console.log(corFavorita);
+
+//let anoNascimento  = prompt('em que ano você nasceu?');
+//console.log(anoNascimento);
+
+//if (anoNascimento > 2007) {
+//    alert('Você ainda é um adolescente!');
+//}
+/*
+let animal = prompt('qual é o seu animal favorito?');
+if (animal == 'cachorro' || animal == 'gato'){
+    alert('Você ama animais!');
+} else {
+    alert('Animais são incríveis!');
+}*/
+/*
+let numero = prompt('insira um número:');
+if (numero > 0){
+    alert('Esse número é positivo.');
+
+} else { if (numero < 0) {
+    alert('este número é negativo.');
+} else{
+    alert('este número é neutro.');
+}
+
+}*/
+/*
+let score = prompt('insert your points:');
+score = score > 50 ? 'Congrats, you win!' : 'Sorry, try again!' ;
+alert(score);*/
+/*
+let saldoDaConta = 182.61;
+alert (`o saldo da sua conta é de R$ ${saldoDaConta}`);
+*/
+/*
+let nome = prompt('Insira seu nome:');
+let sobrenome = prompt('Insira seu sobrenome:');
+alert(`Olá ${nome} ${sobrenome}, tudo certo?`);
+*/
+
+/*let contador = 5;
+while (contador <= 15){
+    console.log(contador);
+    contador++; 
+}*/
+
+/*let contador = 20;
+while(contador >= 10){
+    console.log(contador);
+    contador--;
+}*/
+
+/*let contador = prompt('Insira um número:');
+while(contador >=1 ){
+    console.log(contador);
+    contador-- ;
+}
+let inicio = 1;
+let contador = prompt('insira um número:');
+while(inicio <= contador){
+    console.log(inicio);
+    inicio++;
+}
+
+console.log('Obrigado pelo seu carinho!');*/
+
+let cidade = prompt('qual é sua cidade?');
+console.log(`interessante saber que você é de ${cidade}!`)
